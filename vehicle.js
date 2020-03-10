818a108561b9c104ee15bd6e870e4e8354590b81
Lyve <lyve2909+github@gmail.com>
2020-03-10 22:16:02 +0100

    support actions (door lock/unlock, horn etc)

--- vehicle.js
+++ vehicle.js
@@ -12,13 +12,13 @@ function requestVehicle(_rootData, _vehicleDataCallback, _error)
 
     var vin = _rootData.vin;
 
-    function requestVehicleData(_type, _key)
+    function requestVehicleData(_type, _key, _params)
     {
-        var path = '/api/vehicle/' + _type + '/v1/' + vin;
+        var path = '/api/vehicle/' + _type + '/v1/' + vin + _params;
 
         ++numRequestsPending;
 
-        bmwrequest.call(g_api_host, path, '', function(data)
+        bmwrequest.call(g_api_host, path, null, function(data)
         {
             try
             {
@@ -31,7 +31,7 @@ function requestVehicle(_rootData, _vehicleDataCallback, _error)
             }
             catch (err)
             {
-                _error("Parsing Error: " + err);
+                _error("Parsing Error: " + err + " for data '" + data.toString('hex') + "'");
             }
         }, _error);
     }
@@ -55,54 +55,119 @@ function requestVehicle(_rootData, _vehicleDataCallback, _error)
     requestVehicleData('efficiency', 'efficiency', '');
 }
 
-function requestVehicles (_vehicleDataCallback, _success, _error)
+exports.requestVehicles = function (_vehicleDataCallback, _success, _error)
 {
-    bmwrequest.call(g_api_host, '/api/me/vehicles/v2', '', function(data)
-    {
-		try
+    tokenmanager.initialize(function()
+	{
+		bmwrequest.call(g_api_host, '/api/me/vehicles/v2', null, function(data)
 		{
-            var json = JSON.parse(data);
+			try
+			{
+				var json = JSON.parse(data);
 
-            var numVehicles = json.length;
-            var numVehiclesPending = numVehicles;
+				var numVehicles = json.length;
+				var numVehiclesPending = numVehicles;
 
-            for(var v=0; v<numVehicles; ++v)
-            {
-                requestVehicle(json[v], function(_vin, _data)
-                {
-                    _vehicleDataCallback(_vin, _data);
+				for(var v=0; v<numVehicles; ++v)
+				{
+					requestVehicle(json[v], function(_vin, _data)
+					{
+						_vehicleDataCallback(_vin, _data);
 
-                    if(--numVehiclesPending === 0)
-                    {
-                        _success();
-                    }
-                }, _error);
-            }
-		}
-		catch(err)
-		{
-			_error("Failed to parse data " + data + ", error " + err);
-		}
-    }, _error);
+						if(--numVehiclesPending === 0)
+						{
+							_success();
+						}
+					}, _error);
+				}
+			}
+			catch(err)
+			{
+				_error("Failed to parse data " + data + ", error " + err);
+			}
+		}, _error);
+	}, _error);
 }
 
 exports.writeAllVehiclesData = function (_success, _error)
 {
     tokenmanager.initialize(function()
-        {
-            console.log("Token init completed");
+	{
+		console.log("Token init completed");
 
-            requestVehicles(function(_vin, _data)
-            {
-                var data = JSON.stringify(_data, null, 4);
+		exports.requestVehicles(function(_vin, _data)
+		{
+			var data = JSON.stringify(_data, null, 4);
 
-                var filename = 'result_' + _vin + '.json';
+			var filename = 'result_' + _vin + '.json';
 
-                fs.writeFile(filename, data, function()
-                {
-                    console.log("Result for VIN " + _vin + " written successfully to file " + filename);
-                })
-            }, _success, _error);
-        }, _error
-    );
+			fs.writeFile(filename, data, function()
+			{
+				console.log("Result for VIN " + _vin + " written successfully to file " + filename);
+			})
+		}, _success, _error);
+	}, _error);
 };
+
+exports.executeAction = function (_action, _success, _error)
+{
+/*
+Flash Headlights: RLF
+Sound Horn: RHB
+Lock Door: RDL
+Unlock Door: RDU
+Climate: RCN
+
+Sending Service Message to car: 
+Endpoint: https://www.bmw-connecteddrive.nl/api/vehicle/myinfo/v1
+Type: POST
+Body:
+
+{
+	"vins": ["<VINNUMBER>"],
+	"message": "CONTENT",
+	"subject": "SUBJECT"
+}
+
+
+Get status of command execution: 
+Endpoint: https://www.bmw-connecteddrive.nl/api/vehicle/myinfo/v1
+Type: GET
+
+
+Add News Feed:
+Endpoint: https://www.bmw-connecteddrive.nl/api/me/service/feed/v1
+Type POST
+Body: 
+
+{
+	"url": "http://www.theregister.co.uk/data_centre/headlines.atom",
+	"title": "The Register Data Center"
+}
+*/
+	
+	tokenmanager.initialize(function()
+	{
+		console.log("Token init completed");
+
+		bmwrequest.call(g_api_host, '/api/me/vehicles/v2', null, function(data)
+		{
+			try
+			{
+				var json = JSON.parse(data);
+
+				var vin = json[0].vin;
+				
+				bmwrequest.call(g_api_host, '/api/vehicle/remoteservices/v1/' + vin + '/' + _action, '', function(data)
+				{
+					console.log('Result for action ' + _action + ' for VIN ' + vin + ': ' + data);
+					_success();
+				}, _error);
+			}
+			catch(err)
+			{
+				_error("Failed to parse data " + data + ", error " + err);
+			}
+		}, _error);
+	}, _error);
+}
